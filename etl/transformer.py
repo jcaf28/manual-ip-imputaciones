56add486375dcfd69d9fc3c843e8d6fc541be7f4
JULEN ALVARO ESPINO <jalvaro@caf.net>
2024-10-30 14:21:38 +0100

    falta mes y centro dinámicos

--- etl/transformer.py
+++ etl/transformer.py
@@ -25,17 +25,20 @@ def generar_tabla_imputaciones(descarga_imputaciones, listado_usuarios, wbs_por_
     df = df[df['OBRA_1'].notna()]  # Filtrar filas donde OBRA_1 no es NaN
     df['OBRA_1'] = df['OBRA_1'].astype(int)  # Convertir OBRA_1 a entero
     df = df[df['OBRA_1'] != 0]  # Filtrar filas donde OBRA_1 no es 0
+    df['OBRA_1'] = df['OBRA_1'].astype(str) # Convertir OBRA_1 de vuelta a str para el cruce posterior
+
+
     df = df[df['IdTarea'] != 'E37']
     df = df[df['TAREA'] != 'AUTORIZACIÓN DE SALIDAS Y AUSENCIAS']
 
     # Agrupación de horas por proyecto
-    df = df.groupby(['chapa', 'Clave Obra'])['Horas'].sum().reset_index()
+    df = df.groupby(['chapa', 'OBRA_1'])['Horas'].sum().reset_index()
     
     # Merge con T_LISTADO_USUARIOS para obtener datos de Cost y Cost_2
     df = df.merge(listado_usuarios, left_on='chapa', right_on='IdUsuario', how='left')
     
-    # Merge con T_WBS_por_clave para obtener el WBS correcto en función de Clave Obra y Proceso
-    df = df.merge(wbs_por_clave, left_on='Clave Obra', right_on='PROYECTO BAAN', how='left')
+    # Merge con T_WBS_por_clave para obtener el WBS correcto en función de OBRA_1 y Proceso
+    df = df.merge(wbs_por_clave, left_on='OBRA_1', right_on='PROYECTO BAAN', how='left')
     
     # Modificar la columna "WBS" basado en el valor actual de la columna WBS (FMOP3 o FU300)
     df['WBS'] = df.apply(lambda row: row['WBS PROCESOS (FMOP3)'] 
